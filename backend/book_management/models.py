b42c33b9b446daa0565ded37165cb204918e069c
Emzkie The Great <63085286+usernotfoundy@users.noreply.github.com>
2024-05-15 18:57:43 +0800

    modified the author field

--- backend/book_management/models.py
+++ backend/book_management/models.py
@@ -24,7 +24,7 @@ class Book(models.Model):
     # upload_status = models.BooleanField()
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(Author, on_delete=models.DO_NOTHING, null=True, blank=True)
+    author = models.CharField(max_length=255)
     genre = models.ForeignKey(Genre, on_delete=models.DO_NOTHING, null=True, blank=True)
     book_img = models.ImageField(upload_to='books', blank=True, null=True)
     wishlist = models.BigIntegerField(default=0)
